a4b056fbfdd92f26b96cb21a27a399e8290b07de
Richard Anane Sarfo <richard.sarfo@amalitech.com>
2026-05-11 14:42:35 +0000

    Add Elastic IP and NAT Gateway in public subnet
    NAT lets private workloads pull OS/package updates outbound
    without exposing them to the internet. Placed in public-subnet-1a
    because the NAT Gateway itself needs IGW connectivity.

---
+++ nat_gateway.tf
@@ -0,0 +1,20 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1a.id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
